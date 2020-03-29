1addcc114d87c6e151ba9caa6426cb200cf92dbc
readeral <alan@readeral.com>
2020-03-29 15:41:16 +1100

    updating email to render table

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,17 +1,33 @@
 const sgMail = require('@sendgrid/mail');
 
 export default async function(req, res) {
-  console.log(req);
   sgMail.setApiKey(process.env.XRDS_SENDGRID);
 
   const {targetEmail, message} = req.body;
+
+  const table = Object.entries(message)
+    .map(
+      ([key, value]) =>
+        `<tr><td>${key}</td> <td>${
+          typeof value === 'object'
+            ? `<ul>${value.map(item => `<li>${item}</li>`).join('')}</ul>`
+            : value
+        }</td></tr>`
+    )
+    .join('');
+
+  const emailBody = `
+    <table>
+    ${table}
+    </table>
+    `;
 
   const content = {
     to: targetEmail,
     from: 'info@newfrontdoor.org',
     subject: `[no-reply] New Message From - ${message.email}`,
     text: JSON.stringify(message),
-    html: `<p>${message}</p>`
+    html: `<p>${emailBody}</p>`
   };
 
   try {
