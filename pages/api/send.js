6e26018b4d2e69a8f40d06dcfb8bd72c756c85a9
readeral <alan@readeral.com>
2020-03-29 15:11:33 +1100

    amending props for email

--- pages/api/send.js
+++ pages/api/send.js
@@ -4,13 +4,13 @@ export default async function(req, res) {
   console.log(req);
   sgMail.setApiKey(process.env.XRDS_SENDGRID);
 
-  const {targetEmail, email, message} = req.body;
+  const {targetEmail, message} = req.body;
 
   const content = {
     to: targetEmail,
     from: 'info@newfrontdoor.org',
-    subject: `[no-reply] New Message From - ${email}`,
-    text: message.toString(),
+    subject: `[no-reply] New Message From - ${message.email}`,
+    text: JSON.stringify(message),
     html: `<p>${message}</p>`
   };
 
