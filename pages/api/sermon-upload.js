b2e7ff862ce8de24896fb60331de59ff64479bcd
readeral <alan@readeral.com>
2020-10-22 15:20:00 +1100

    adding sermon upload api route

---
+++ pages/api/sermon-upload.js
@@ -0,0 +1,56 @@
+import S3 from 'aws-sdk/clients/s3';
+import mime from 'mime';
+import cryptoRandomString from 'crypto-random-string';
+
+const createPresignedPost = params => {
+  const s3 = new S3({region: 'ap-southeast-2'}); // Set the region according to the bucket's needs
+
+  return new Promise((resolve, reject) => {
+    s3.createPresignedPost(params, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  });
+};
+
+export default async function({name}) {
+  const contentType = mime.getType(name);
+  const key = `${cryptoRandomString({length: 16, type: 'url-safe'})}_${name}`;
+  const bucket = 'sermons.crossroadshobart.org';
+  const origin = 'https://crossroadshobart.sanity.studio/dashboard';
+
+  const params = {
+    Expires: 60,
+    Bucket: bucket,
+    Conditions: [['content-length-range', 100, 100000000]], // 100Byte - 10MB
+    Fields: {
+      'Content-Type': contentType,
+      key
+    }
+  };
+
+  try {
+    const presignedPostData = await createPresignedPost(params);
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: false,
+        data: presignedPostData,
+        message: null
+      })
+    };
+
+    return [null, response];
+  } catch (error) {
+    return [error];
+  }
+}
