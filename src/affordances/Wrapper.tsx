26ad8fc0a969740700d8e7a55595a698cf5aacf2
Allan Hortle <allanhortle@gmail.com>
2025-03-04 08:31:57 +1100

    Types

--- src/affordances/Wrapper.tsx
+++ src/affordances/Wrapper.tsx
@@ -1,6 +1,6 @@
-import React, {ReactElement} from 'react';
+import type {ReactElement, ReactNode} from 'react';
 
-export default function Wrapper(props: {}): ReactElement {
+export default function Wrapper(props: {children: ReactNode}): ReactElement {
     return (
         <div className="max-w-2xl m-auto grid grid-cols-5 text-sm items-center">
             {props.children}
