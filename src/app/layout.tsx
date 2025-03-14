bdbf8cadd843ecfac2abd67340e8e2ba6f74df19
Allan Hortle <allanhortle@gmail.com>
2025-03-14 18:42:01 +1100

    Church camp banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,66 +4,66 @@ import Navigation from './Navigation';
 import Link from 'next/link';
 
 const defaultUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
 
 export const metadata = {
-    metadataBase: new URL(defaultUrl),
-    title: {
-        template: '%s — Crossroads Presbyterian Church',
-        default: 'Crossroads Presbyterian Church'
-    },
-    description: `We are an afternoon church in the city of Hobart. Our message is Jesus Christ and finding God through him. Kid's, Youth and Young Adult Ministries are important to us.`
+  metadataBase: new URL(defaultUrl),
+  title: {
+    template: '%s — Crossroads Presbyterian Church',
+    default: 'Crossroads Presbyterian Church'
+  },
+  description: `We are an afternoon church in the city of Hobart. Our message is Jesus Christ and finding God through him. Kid's, Youth and Young Adult Ministries are important to us.`
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-    return (
-        <html
-            lang="en"
-            className="bg-amber-50 text-sky-950 font-sans font-normal text-lg leading-6"
-        >
-            <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-                    rel="stylesheet"
-                />
-                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-            </head>
-            <body className="flex flex-col min-h-screen">
-                <Navigation />
-                <main className="flex-1 px-4">{children}</main>
+  return (
+    <html lang="en" className="bg-amber-50 text-sky-950 font-sans font-normal text-lg leading-6">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
+      <body className="flex flex-col min-h-screen">
+        <div className="bg-red-500 text-white text-center p-2">
+          There will be no Sunday service on March 23 due to church camp.
+        </div>
+        <Navigation />
+        <main className="flex-1 px-4">{children}</main>
 
-                <footer className="leading-4 p-6 md:p-16 mt-32 bg-[#ede7d1]">
-                    <div className="max-w-2xl m-auto flex flex-col gap-2 justify-start md:grid grid-cols-5 text-sm md:items-center">
-                        <span className="col-span-3">
-                            © Crossroads Presbyterian Church {new Date().getFullYear()}
-                            <br />
-                            25 Tasma St, North Hobart TAS 7000, Australia
-                        </span>
-                        <div>
-                            <strong>Resources</strong>
-                            <ul>
-                                <li>
-                                    <Link href="/giving">Giving</Link>
-                                </li>
-                                <li>
-                                    <Link href="/sermons">Sermons</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <strong>Policies</strong>
-                            <ul>
-                                <li>&nbsp;</li>
-                                <li>&nbsp;</li>
-                            </ul>
-                        </div>
-                    </div>
-                </footer>
-                <GoatCounter />
-            </body>
-        </html>
-    );
+        <footer className="leading-4 p-6 md:p-16 mt-32 bg-[#ede7d1]">
+          <div className="max-w-2xl m-auto flex flex-col gap-2 justify-start md:grid grid-cols-5 text-sm md:items-center">
+            <span className="col-span-3">
+              © Crossroads Presbyterian Church {new Date().getFullYear()}
+              <br />
+              25 Tasma St, North Hobart TAS 7000, Australia
+            </span>
+            <div>
+              <strong>Resources</strong>
+              <ul>
+                <li>
+                  <Link href="/giving">Giving</Link>
+                </li>
+                <li>
+                  <Link href="/sermons">Sermons</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <strong>Policies</strong>
+              <ul>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+        <GoatCounter />
+      </body>
+    </html>
+  );
 }
